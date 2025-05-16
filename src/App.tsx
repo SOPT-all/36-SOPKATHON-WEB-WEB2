a20a38903bc4fa7837682b5e16c42c53e887a12c
jisooooooooooo <dlawltn0708@naver.com>
2025-05-16 15:20:49 +0900

    feat: 스타일 리셋

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./shared/router/Router";
-import "./App.css";
 
 function App() {
   return <RouterProvider router={router} />;
