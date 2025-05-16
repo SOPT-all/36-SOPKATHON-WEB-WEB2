df9a532922184bf11af643c18f8fba93410ddfb5
jisooooooooooo <dlawltn0708@naver.com>
2025-05-16 15:15:33 +0900

    init: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./shared/router/Router";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 export default App;
---
+++ src/shared/router/Router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "@/pages/main/page/MainPage";
+import Page1 from "@/pages/page1/page/Page1";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/page1",
+    element: <Page1 />,
+  },
+]);
+
+export default router;
