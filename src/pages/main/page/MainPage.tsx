309b7839e3e674c46fe473ef0609b12369f5e945
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-05-18 02:06:00 +0900

    pin, navermap 구현

--- src/pages/main/page/MainPage.tsx
+++ src/pages/main/page/MainPage.tsx
@@ -1,10 +1,10 @@
-import NaverMap from '@/shared/components/NaverMap';
+import NaverMap from '@/shared/components/naverMap/NaverMap';
 
 const MainPage = () => {
   return (
     <div className="w-full h-screen">
       <div className="w-[23.4375rem] h-[41.6875rem] mx-auto flex-shrink-0">
-        <NaverMap useCurrentLocation={true} className="w-full h-full" />
+        <NaverMap useCurrentLocation={false} className="w-full h-full" />
       </div>
     </div>
   );
