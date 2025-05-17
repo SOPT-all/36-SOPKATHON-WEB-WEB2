21534c5704767a24217524b7255d24ce5b64535a
jisooooooooooo <dlawltn0708@naver.com>
2025-05-18 05:45:20 +0900

    feat: 장소 상세 페이지 API 연동

--- src/shared/apis/apiClient.ts
+++ src/shared/apis/apiClient.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// 실제 API URL로 바꾸기
-const BASE_URL = 'https://web3.com';
+const BASE_URL = 'https://13.124.240.84.nip.io';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
